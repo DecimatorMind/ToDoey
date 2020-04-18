0b97ee3a4b0395cb8819b644f088921efeebafe1
Pranjal Bhardwaj <E19CSE432@bennett.edu.in>
2020-04-18 07:28:35 +0530

    Persistent Storage added

---
+++ ToDoey/Controllers/ToDoListViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  ToDoey
+//
+//  Created by Pranjal Bhardwaj on 18/04/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        print(dataFilePath!)
+        
+        loadItems()
+        
+    }
+
+    //MARK: - TableView Data Source Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+    
+        return cell
+        
+    }
+
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add a new ToDoey item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create new Item"
+            
+            textField = alertTextField
+        }
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            //what will happen when the user clicks the add button
+            
+            let newItem = Item()
+            
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+        }
+
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func saveItems(){
+        
+        let encoder = PropertyListEncoder()
+            
+            do {
+                
+                let data = try encoder.encode(itemArray)
+                
+                try data.write(to: dataFilePath!)
+            } catch {
+                print("error encoding \(error)")
+            }
+            self.tableView.reloadData()
+        }
+
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding \(error)")
+            }
+        }
+    }
+
+
+}
+
---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Pranjal Bhardwaj on 18/04/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
